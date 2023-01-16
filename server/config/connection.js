0b9841b542c3c2c31f12ee8dccc2e94bab4352da
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-01-16 12:42:06 -0500

    server running database test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-
-mongoose.connect( `mongodb+srv://lambdauser:root1234@cluster0.i1qni6a.mongodb.net/?retryWrites=true&w=majority`, {
-  useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+
+// mongoose.connect( `mongodb+srv://lambdauser:root1234@cluster0.i1qni6a.mongodb.net/?retryWrites=true&w=majority`, {
+//   useNewUrlParser: true, useUnifiedTopology: true
+// });
 
 module.exports = mongoose.connection;
