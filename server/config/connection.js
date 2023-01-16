1b7263c423b269cd823a5107b74e26ee93d3b6a4
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-01-16 09:35:47 -0500

    test config EC2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+let MONGODB_URI= `mongodb+srv://lambdauser:root1234@cluster0.i1qni6a.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
   useNewUrlParser: true,
